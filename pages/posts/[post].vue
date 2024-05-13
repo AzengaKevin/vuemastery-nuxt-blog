c7bfdcffa539d4b1b229c1eb5659a17ee81c8690
Azenga Kevin <azenga.kevin7@gmail.com>
2024-05-13 04:45:02 +0300

    wip: post category page

---
+++ pages/posts/[post].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { PostDetails } from '@/data/posts'
+import { getPostDetailsUrl } from '@/data/posts'
+
+const postSlug = useParam('post')
+
+const { data: post } = await useFetch<PostDetails>(getPostDetailsUrl(postSlug))
+</script>
+<template>
+  <main>
+    <template v-if="post">
+      <h1>{{ post?.title }}</h1>
+
+      <CategoryLink :category="post?.category" />
+      <RenderMarkdown :source="post?.content" />
+    </template>
+  </main>
+</template>
