e8ccd191cfef0a7740bdd340764135e58f3c8802
Azenga Kevin <azenga.kevin7@gmail.com>
2024-05-13 03:53:37 +0300

    wip: extracting a post-card component

--- pages/index.vue
+++ pages/index.vue
@@ -8,37 +8,7 @@ const { data: posts } = await useFetch<Post[]>(getPostsUrl())
   <main>
     <h1>Vuemastery Nuxt Blog</h1>
     <div>
-      <div
-        v-for="{ id, title, category, intro } in posts"
-        :key="id"
-        class="card"
-      >
-        <h2 class="title">{{ title }}</h2>
-        <div class="category">
-          <a href="#">Category: {{ category.name }}</a>
-        </div>
-        <RenderMarkdown :source="intro" />
-      </div>
+      <PostCard v-for="post in posts" :key="post.id" :post="post" />
     </div>
   </main>
 </template>
-
-<style scoped lang="scss">
-@use '@/assets/styles/colors.scss';
-
-.card {
-  padding: 15px;
-  border: 1px solid colors.$green-dark;
-  border-radius: 5px;
-  margin-bottom: 25px;
-  font-size: 12pt;
-}
-
-.card .title {
-  font-size: 20pt;
-}
-
-.card .category {
-  margin: 5px 0px;
-}
-</style>
